97550b5b90fb8c951b52394be44122610e4c940b
Joanna Schroeder <js2mr@virginia.edu>
2022-07-12 14:47:25 -0400

    data format

--- data/txt_files/cleaning.R
+++ data/txt_files/cleaning.R
@@ -1,5 +1,7 @@
 library(tm)
 library(stringr)
+library(tidytext)
+library(tidyverse)
 
 #importing first document
 doc1 <-paste(readLines("DedooseDoc_AR_623-23.docx_2022_7_7_1258.txt"), collapse = " ")
@@ -11,5 +13,10 @@ doc1 <- tolower(doc1)
 doc1 <- removeWords(doc1, stopwords())
 doc1 <- gsub(pattern = "\\b[A-z]\\b{1-3}", replace= " ", doc1)
 doc1 <- stripWhitespace(doc1)
-doc1 <- str(str_split(doc1, pattern = "\\s+"))
+#doc1 <- str_split(doc1, pattern = "\\s+")
+
+
+text_df <- tibble(document = "AR623", text = c(doc1))
+text_df %>% unnest_tokens(word, text) %>% count(document, word, sort = TRUE)
+
 
