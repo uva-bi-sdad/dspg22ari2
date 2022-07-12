4577af0075cd0811cd4c958beae64eeb9f0d63c2
Skylar Haskiell <seh6fy@virginia.edu>
2022-07-12 14:26:35 -0400

    Joanna

---
+++ data/txt_files/cleaning.R
@@ -0,0 +1,15 @@
+library(tm)
+library(stringr)
+
+#importing first document
+doc1 <-paste(readLines("DedooseDoc_AR_623-23.docx_2022_7_7_1258.txt"), collapse = " ")
+#cleaning document
+
+doc1 <- gsub(pattern="\\W" , replace = " ", doc1)
+doc1 <- gsub(patter = "\\d", replacement = " ", doc1)
+doc1 <- tolower(doc1)
+doc1 <- removeWords(doc1, stopwords())
+doc1 <- gsub(pattern = "\\b[A-z]\\b{1-3}", replace= " ", doc1)
+doc1 <- stripWhitespace(doc1)
+doc1 <- str(str_split(doc1, pattern = "\\s+"))
+
