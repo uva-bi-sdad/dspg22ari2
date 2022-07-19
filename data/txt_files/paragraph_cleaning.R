2b23897a4007c4786df962dd647bedac60fd34bc
Joanna Schroeder <js2mr@virginia.edu>
2022-07-19 14:58:30 -0400

    forgot about stringi, adding better comments

--- data/txt_files/paragraph_cleaning.R
+++ data/txt_files/paragraph_cleaning.R
@@ -8,6 +8,7 @@ library(widyr)
 library(ggplot2)
 library(igraph)
 library(ggraph)
+library(stringi)
 
 #importing documents, removing analysis.R, cleaning.R and paragraph_cleaning.R
 files <- list.files("~/git/dspg22ari2/data/txt_files")
@@ -15,19 +16,18 @@ files <- files[!grepl("R$", files)]
 names <- NULL
 cleaned <- NULL
 for(file in files){
+  # nothing after readLines, reads it in as paragraphs
   doc01 <-paste(readLines(file))
-# doc1 <- pdf_ocr_text(files[1])
-  #doc1 <- str_split(doc01, pattern = "")
   #cleaning document
   doc01 <- gsub(pattern="\\W" , replace = " ", doc01)%>% gsub(patter = "\\d", replacement = " ", doc01)
-  doc01<- removeWords(doc01, c(stopwords("english"),"will","can","on","table","da","other","only", "the", "other", "others", "The"))
+  doc01<- removeWords(doc01, c(stopwords("english"),"will","can","on","table","da","other","only", "the", "other", "others", "The")) # adding capital T to the stopwords
   doc01 <- gsub(pattern = "\\b[A-z]\\b{1-3}", replace= " ", doc01)
   doc01 <- stripWhitespace(doc01)
-  doc01 <- stringi::stri_remove_empty(doc01)
-  cleaned <- cleaned %>% append(doc01)
-  names <- names %>% append(rep(file, length(doc01)))
+  doc01 <- stringi::stri_remove_empty(doc01) # remove blank paragraphs
+  cleaned <- cleaned %>% append(doc01) # object for cleaned paragraph text
+  names <- names %>% append(rep(file, length(doc01))) # object for the document name each paragraph belongs to
 }
-#doc1 <- stringi::stri_remove_empty(doc01)
+
 text_df <- tibble(paragraph = c(seq(1:length(cleaned))), text = cleaned, document = names)
 tokenized_paragraphs <- text_df %>% unnest_tokens(word, text) %>% mutate(stem = wordStem(word)) %>% count(stem, paragraph, sort = TRUE)
 cooccurence <- tokenized_paragraphs %>% pairwise_count(stem, paragraph, wt = n, sort = TRUE, upper = FALSE)
