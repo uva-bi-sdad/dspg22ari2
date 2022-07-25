f49c13e39ed4d8aa8c553cb402f4296f43ae013f
Skylar Haskiell <seh6fy@virginia.edu>
2022-07-25 12:22:47 -0400

    this time work

--- data/txt_files/paragraph_cleaning.R
+++ data/txt_files/paragraph_cleaning.R
@@ -9,35 +9,51 @@ library(ggplot2)
 library(igraph)
 library(ggraph)
 library(stringi)
+library(pacman)
+library(textstem)
+library(qdapRegex)
 
 #importing documents, removing analysis.R, cleaning.R and paragraph_cleaning.R
 files <- list.files("~/git/dspg22ari2/data/txt_files")
 files <- files[!grepl("R$", files)]
+files <- files[!grepl("RDS$", files)]
 names <- NULL
 cleaned <- NULL
+# my_stop_words <- bind_rows(stop_words, tibble(word = c("bolc","tmts","cohen","hambrick","ancona","ruth","ross","johnson","rodriguez","ibid","adrp","also","bunnell","cajina","rc","ccc","ile","ar","finkelstein",as.character(1:12)), lexicon = rep("custom", 30)))
+
 for(file in files){
   # nothing after readLines, reads it in as paragraphs
   doc01 <-paste(readLines(file))
   #cleaning document
-  doc01 <- gsub(pattern="\\W" , replace = " ", doc01)%>% gsub(patter = "\\d", replacement = " ", doc01)
-  doc01<- removeWords(doc01, c(stopwords("english"),"will","can","on","table","da","other","only", "the", "other", "others", "The")) # adding capital T to the stopwords
-  doc01 <- gsub(pattern = "\\b[A-z]\\b{1-3}", replace= " ", doc01)
+  doc01 <- gsub(pattern="\\W" , replace = " ", doc01)%>% gsub(pattern = "\\d", replacement = " ", doc01)
+  doc01<-tolower(doc01)
+  doc01<- lemmatize_strings(doc01)
+  doc01 <- rm_nchar_words(doc01, "1,3")
+  # doc01 <- gsub('\\b\\w{1,3}\\b','',doc01)
   doc01 <- stripWhitespace(doc01)
   doc01 <- stringi::stri_remove_empty(doc01) # remove blank paragraphs
+  # doc01 <- doc01 %>% anti_join(my_stop_words)
+  doc01<- removeWords(doc01, c(stopwords("english"),"bolc","tmts","cohen","hambrick",
+        "ancona","ruth","ross","johnson","rodriguez","ibid","adrp","also","bunnell",
+        "cajina","rc","ccc","ile","ar","finkelstein","capl","brown","christiansen",
+        "fema","kingston","mcpherson","opms","hqda","arng","wocs","capi","opmg",
+        "much", "will")) # adding capital T to the stopwords
   cleaned <- cleaned %>% append(doc01) # object for cleaned paragraph text
   names <- names %>% append(rep(file, length(doc01))) # object for the document name each paragraph belongs to
 }
 
 text_df <- tibble(paragraph = c(seq(1:length(cleaned))), text = cleaned, document = names)
-tokenized_paragraphs <- text_df %>% unnest_tokens(word, text) %>% mutate(stem = wordStem(word)) %>% count(stem, paragraph, sort = TRUE)
-cooccurence <- tokenized_paragraphs %>% pairwise_count(stem, paragraph, wt = n, sort = TRUE, upper = FALSE)
+tokenized_paragraphs <- text_df %>% unnest_tokens(word, text) %>% count(word, paragraph, sort = TRUE)
+tokenized_words <- text_df %>% unnest_tokens(word,text) %>% count(word,document, sort = TRUE)
+cooccurence <- tokenized_paragraphs %>% pairwise_count(word, paragraph, wt = n, sort = TRUE, upper = FALSE)
 
-cooccurence %>%
-  filter(n >=350) %>%
-  graph_from_data_frame() %>%
-  ggraph(layout = "fr") +
-  geom_edge_link(aes(edge_alpha = n, edge_width = n), edge_colour = "cyan4") +
-  geom_node_point(size = 5) +
-  geom_node_text(aes(label = name), repel = TRUE,
-                 point.padding = unit(0.2, "lines")) +
-  theme_void()
+# cooccurence %>%
+#   filter(n >=1500) %>%
+#   graph_from_data_frame() %>%
+#   ggraph(layout = "fr") +
+#   geom_edge_link(aes(edge_alpha = n, edge_width = n), edge_colour = "cyan4") +
+#   geom_node_point(size = 3) +
+#   geom_node_text(aes(label = name), repel = TRUE,
+#                  point.padding = unit(0.2, "lines")) +
+#   theme_void()
+
